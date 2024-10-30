29432b72f5fa122c10a8736366fb1a6255fe90cd
Jack-WessANU <84662674+Jack-WessANU@users.noreply.github.com>
2024-10-30 17:22:31 +1100

    Update GO_enrichment_IPvsIV.R

--- GO_enrichment_IPvsIV.R
+++ GO_enrichment_IPvsIV.R
@@ -20,7 +20,6 @@ library(lattice)
 library(dplyr)
 library(Rgraphviz)
 
-
 # Loading Data
 # create GO background mapping
 # using dataframe "penx7.csv" with two columns, gene name and GO terms
@@ -37,13 +36,11 @@ geneNames
 # Basically this is subsetting down genes based on whether they are significantly upregulated, maybe go higher than 1 LFC but lets see
 myInterestingGenes <- subset(IPvsIV, adj.P.Val < 0.05 & logFC > 1)$Column1
 
-
 # create selection and gene list object
 geneList <- factor(as.integer(geneNames %in% myInterestingGenes)) 
 names(geneList) <- geneNames
 str(geneList)
 
-View(geneList)
 # create topgo object
 my_go_data <- new("topGOdata",
                   ontology    = "BP",
@@ -53,9 +50,7 @@ my_go_data <- new("topGOdata",
                   nodeSize    = 5) # nodesize is min number of genes in GO_term
 my_go_data
 
-
 sigGenes(my_go_data) # see sig DE genes
-
 
 ## Enrichment Tests
 # use fisher for gene set number enrichment (not using gene values)
@@ -72,7 +67,6 @@ result_weight_output$weight_fisher_adjusted <- p.adjust(p = result_weight_output
 # P-value distribution from Fisher test
 ggplot(result_weight_output, aes(x = as.numeric(weight_fisher))) + geom_histogram()
 
-
 # Create map of sig GO terms
 par(cex = 0.35) # change font size
 showSigOfNodes(my_go_data, score(result_weight_fisher), firstSigNodes = 5, useInfo = 'all') #firstSigNodes selects how many sig GO_terms
@@ -85,8 +79,6 @@ genes
 # get sig GO terms
 
 sig_terms <- result_weight_output[as.numeric(result_weight_output$weight_fisher) < 0.05,] # or fisher adjusted values
-
-sig_terms
 
 # Code to generate a dotplot of enriched GO terms, with Gene Ratio (meaning number of significant genes within an individual GO term) as the x axis and GO term as the y axis. The dot size is relative to the total number of significant genes in the term and its colour
 # reflects the significance of the term overall (Fisher test)
@@ -102,8 +94,6 @@ for(go_term in sig_terms$GO.ID){
   sig_genes <- rbind(sig_genes, sig_gene)
 }
 
-
-
 sig_terms_genes <- right_join(sig_terms, sig_genes, by = "GO.ID") # merge genes with result object above
 
 # associate LFC with sig genes
@@ -112,9 +102,6 @@ sig_LFC <- right_join(IVvsIP, sig_terms_genes, by = "Column1")
 # order by most sig GO term
 sig_LFC <- sig_LFC[order(as.numeric(sig_LFC$weight_fisher)),]
 sig_LFC$Term <- factor(sig_LFC$Term , levels = rev(unique(sig_LFC$Term )))
-
-sig_terms_genes
-
 
 # plot LFC
 # use "GO.ID" if GO "Term" is not unique
